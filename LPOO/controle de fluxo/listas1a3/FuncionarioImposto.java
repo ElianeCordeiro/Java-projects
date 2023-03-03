a324922eea91d6619ba09ea5efd7c4806956a6d8
elian <elianemcordeiro1@gmail.com>
2023-03-03 09:53:14 -0300

    Estruturas de controle de fluxo

---
+++ LPOO/controle de fluxo/listas1a3/FuncionarioImposto.java
@@ -0,0 +1,15 @@
+package listas1a3;
+
+import java.util.Scanner;
+
+public class FuncionarioImposto {
+
+	public static void main(String[] args) {
+		
+		Scanner read = new Scanner(System.in);
+		
+		System.out.println("");
+		
+	}
+
+}
